3909b0bd101dcb8763fd1d625cd171f6e5e2a714
johnny <johnnye.cuevas@hotmail.com>
2020-01-10 20:35:58 -0500

    added file type distinction, error handling for duplicate files

--- FolderFix.py
+++ FolderFix.py
@@ -1,31 +1,42 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
 import os
-import json
 import time
 
+
 class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
+    def on_any_event(self, event):
         for filename in os.listdir(folder_tracked):
             src = folder_tracked + "\\" + filename
-            new_destination = folder_destination + "\\" + filename
-            os.rename(src, new_destination)
-            return folder_tracked
+            if filename.endswith(".exe"):
+                new_destination = folder_tracked + "\\Programs\\" + filename
+            elif filename.endswith(".rmskin"):
+                new_destination = folder_tracked + "\\Rainmeter Skins\\" + filename
+            elif filename.endswith(tuple([".pdf", ".doc", ".docx", ".txt"])):
+                new_destination = folder_tracked + "\\Documents\\" + filename
+            elif filename.endswith(tuple([".mp4", ".wav", ".flac", ".mp3"])):
+                new_destination = folder_tracked + "\\Music\\" + filename
+            else:
+                continue
+            try:
+                file_mover(src, new_destination)
+            except FileExistsError:
+                change = new_destination.split(".")
+                file_mover(src, change[0] + " copy" + change[1])
 
 
-folder_tracked = "F:\\Downloads\\practice"
-folder_destination = "F:\\Downloads\\Programs\\practice"
+def file_mover(src, destination):
+    os.rename(src, destination)
+
+
+folder_tracked = "F:\\Downloads"
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_tracked, recursive=True)
 observer.start()
-
 try:
     while True:
-        time.sleep(10)
-        print("works")
-
+        time.sleep(2)
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
