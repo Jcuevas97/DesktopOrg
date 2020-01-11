536b5824e5281f32a7d6030e954b3c2898bf55f8
johnny <johnnye.cuevas@hotmail.com>
2020-01-10 19:02:09 -0500

    Corrected forward slashes to escaped backslash for usage in windows

--- FolderFix.py
+++ FolderFix.py
@@ -8,14 +8,14 @@ import time
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_tracked):
-            src = folder_tracked + "/" + filename
-            new_destination = folder_destination + "/" + filename
+            src = folder_tracked + "\\" + filename
+            new_destination = folder_destination + "\\" + filename
             os.rename(src, new_destination)
             return folder_tracked
 
 
-folder_tracked = "F:\Downloads\practice"
-folder_destination = "F:\Downloads\Programs\practice"
+folder_tracked = "F:\\Downloads\\practice"
+folder_destination = "F:\\Downloads\\Programs\\practice"
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_tracked, recursive=True)
